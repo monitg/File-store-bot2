7e2497fd891d5a84d7876dceaa12943237511276
monitg <142651228+monitg@users.noreply.github.com>
2025-02-21 11:26:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "60"))
 #your start default command message.
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link. 💾")
 #your telegram tag without @
-OWNER_TAG = os.environ.get("OWNER_TAG", "Thala_dev")
+OWNER_TAG = os.environ.get("OWNER_TAG", "Thala_Dev")
 #Time in seconds for message delete, put 0 to never delete
 TIME = int(os.environ.get("TIME", "60"))
 
